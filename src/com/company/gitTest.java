77d9c7ec6e096960147162a5df8cd10cac54966c
Jacob <jacobmreinert@gmail.com>
2018-05-01 07:48:27 -0500

    changed class name and added fruit counter

--- src/com/company/gitTest.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class gitTest {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        //comment
-    }
-}
---
+++ src/com/company/newInternet.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class newInternet {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        //comment
+        int apples = 5;
+        int oranges = 6;
+        int total;
+
+        total = apples + oranges;
+
+        System.out.println(total);
+
+    }
+}
