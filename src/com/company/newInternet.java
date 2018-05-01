49c24affb8f336c41e49b21ee081339722728d08
Jacob R <jacobmreinert@gmail.com>
2018-05-01 08:57:02 -0500

    added comments and cleaned up code

--- src/com/company/newInternet.java
+++ src/com/company/newInternet.java
@@ -5,22 +5,25 @@ public class newInternet {
     public static void main(String[] args) {
         System.out.println("hello world");
         //comment
-        int apples = 5;
-        int oranges = 6;
+        int apples;//will be initialized when prompted
+        int oranges;//will be initialized when prompted
         int total;
         Scanner scnr = new Scanner(System.in);
 
+        //get number of apples
         System.out.println("Enter number of apples: ");
         apples = scnr.nextInt();
 
+        //get number of oranges
         System.out.println("Enter number of oranges: ");
         oranges = scnr.nextInt();
 
-
+        //calculate total fruit
         total = apples + oranges;
 
-        System.out.println(total);
-        System.out.println("HELLO FROM DESKTOP");
+        //print total
+        System.out.println("Total peices of fruit:" + total);
+
 
     }
 }
